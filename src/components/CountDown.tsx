41521941da73f25a8c1d0f20eaadd46dd3de1151
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-02-24 19:05:23 -0300

    feat: concluindo o countDown, com as funcionalidades de encerrar o ciclo e as estilizaçoes

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,31 +2,49 @@ import { useEffect, useState } from "react";
 
 import styles from "../styles/components/CountDown.module.css";
 
+let countDownTimeOut: NodeJS.Timeout;
+
 export function CountDown() {
-  const [time, setTime] = useState(25 * 60); //25minutos * 60segundos
-  const [active, setActive] = useState(false); //estado que armazena se o countDown está ativo ou não
+  const [time, setTime] = useState(0.1 * 60); //25minutos * 60segundos
+  const [isActive, setIsActive] = useState(false); //estado que armazena se o countDown está ativo ou não
+  const [hasFinished, setHasFinished] = useState(false); //state pra quando o countDown tiver finalizado
 
   const minutes = Math.floor(time / 60); //número de minutos, arredondando pra baixo
   const seconds = time % 60; //pegando o "resto", ex: 17:32, 32: resto
 
-  const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
-  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split(''); /*
+  const [secondsLeft, secondsRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
+  const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, "0")
+    .split(
+      ""
+    ); /*
     transformando o "minutes" em String()
     .padStart(2, '0'): verifica se há dois digitos, ex: 25, se houver faz o split(''). Se não acrescenta o '0' a esquerda
     split(''): separa os dois digítos, ex: '2' '5'.
   */
 
-  function startCountDown(){
-    setActive(true); //vai startar o countDown
+  function startCountDown() {
+    setIsActive(true); //vai startar o countDown
+  }
+
+  function resetCountDown() {
+    clearTimeout(countDownTimeOut); //cancelando a execução do setTimeout
+    setIsActive(false); //vai parar o countDown
+    setTime(0.1 * 60); //zerando o contador
   }
 
   useEffect(() => {
-    if(active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countDownTimeOut = setTimeout(() => {
         setTime(time - 1); //reduz um segundo do tempo
-      }, 1000) //espera um segundo
+      }, 1000); //espera um segundo
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -42,13 +60,35 @@ export function CountDown() {
         </div>
       </div>
 
-      <button 
-        type="button" 
-        className={styles.countDownButton} 
-        onClick={startCountDown}
-      >
-        Iniciar um ciclo
-      </button>
+      {hasFinished ? (
+        <button
+          disabled 
+          type="button"
+          className={`${styles.countDownButton}`}
+          onClick={resetCountDown}
+        >
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button 
+              type="button"
+              className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countDownButton}
+              onClick={startCountDown}
+            >
+              Iniciar ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
