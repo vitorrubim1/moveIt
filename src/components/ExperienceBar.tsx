fb8a64f981971d29f8ac06848fec22abe4592b16
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-02-25 17:08:06 -0300

    fix: ajustando o estilo variavel da barra de experiencia

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,30 @@
-import styles from '../styles/components/ExperienceBar.module.css';
+import { useContext } from "react";
+
+import { ChallengesContext } from "../contexts/ChallengeContext";
+
+import styles from "../styles/components/ExperienceBar.module.css";
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext
+  );
+
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel; //regra de três
+
   return (
     <header className={styles.experienceBar}>
       <span>0xp</span>
       <div>
-        <div style={{ width: "50%" }} /> {/*estilo variável*/}
-        <span className={styles.currentExperience} style={{ left: "50%" }}>
-          300xp
+        <div style={{ width: `${percentToNextLevel}%` }} /> {/*estilo variável*/}
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${percentToNextLevel}%` }}
+        >
+          {currentExperience} xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
