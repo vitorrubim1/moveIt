40dda875e6716756f0a87a76664726f8d2d7bba2
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-02-25 18:15:59 -0300

    chore: colocando o countDown somente no index, pq quero compartilhar dados e funcoes somente para componentes especificos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,30 @@ import { CountDown } from "../components/CountDown";
 import { Profile } from "../components/Profile";
 
 import styles from "../styles/pages/Home.module.css";
+import { CountDownProvider } from "../contexts/CountDownContext";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Inícion | moveIt</title>
+        <title>Início | move.it</title>
       </Head>
 
       <ExperienceBar />
 
-      <section>
-        <div>{/*primeira coluna*/}
-          <Profile />
-          <CompletedChallenges />
-          <CountDown />
-        </div>
+      <CountDownProvider> {/*contexto do countDown só está por volta dos componentes que utiliza informações do contexto dele*/}
+        <section>
+          <div> {/*primeira coluna*/}
+            <Profile />
+            <CompletedChallenges />
+            <CountDown />
+          </div>
 
-        <div>{/*segunda coluna*/}
-          <ChallengeBox />
-        </div>
-      </section>
+          <div> {/*segunda coluna*/}
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountDownProvider>
     </div>
   );
 }
